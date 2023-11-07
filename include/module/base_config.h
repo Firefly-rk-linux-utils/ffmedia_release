fb8de890a3dc81d36770c410f688369aad71de6c
dengkx <dkx@t-chip.com.cn>
2023-11-07 17:04:22 +0800

    firefly: Support armhf

--- include/module/base_config.h
+++ include/module/base_config.h
@@ -1,10 +1,10 @@
 #ifndef __BASE_CONFIG_H__
 #define __BASE_CONFIG_H__
 
-#define	FFMEDIA_VERSION             "v2.2.1"
+#define	FFMEDIA_VERSION             "v2.2.1-5-g8c87d7b"
 
-#define AUDIO_SUPPORT               1
-#define OPENGL_SUPPORT              1
+#define AUDIO_SUPPORT               0
+#define OPENGL_SUPPORT              0
 
 #endif
 
